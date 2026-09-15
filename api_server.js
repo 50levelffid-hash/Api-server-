4523e5eb7231f38edecb46c135e24c811a8a29a7
50levelffid-hash <50levelffid@gmail.com>
2026-09-15 15:14:45 +0530

    Update api_server.js

--- api_server.js
+++ api_server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // ============================================================
 
 const MONGODB_URL = "mongodb+srv://sahajada07:Sahajada123@cluster0.vynn0ht.mongodb.net/?appName=Cluster0";
-const DB_NAME = "otp_bomber";
+const DB_NAME = "otp_bomb";
 
 const MAX_EFFECTIVE_DURATION = 10;
 const FIRST_RUN_RETRY = 1;
